47b8810563a0dff2cbf4494c77dcd9a81ca75b7f
ifyouknow-youknow <bagel.inbox@gmail.com>
2024-11-30 21:12:36 -0800

    1

--- src/COMPONENTS/Clickable.js
+++ src/COMPONENTS/Clickable.js
@@ -1,5 +1,4 @@
 export function Clickable({ onPress, children }) {
-
     return <div className="pointer fit-width" onClick={() => onPress !== undefined ? onPress() : console.log("YOU PRESSED ME!")}>
         {children}
     </div>
