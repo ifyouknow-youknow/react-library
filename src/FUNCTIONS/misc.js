d076a256f2297f4e81a5bb5b611df182918230a8
ifyouknow-youknow <bagel.inbox@gmail.com>
2024-12-15 01:02:26 -0800

    mach 43

--- src/FUNCTIONS/misc.js
+++ src/FUNCTIONS/misc.js
@@ -7,3 +7,11 @@ export function randomString(length) {
     }
     return result;
 }
+
+export function emailFix(isWithPrefix, email, appName) {
+    if (isWithPrefix) {
+        return `${appName.toLowerCase()}_${email}`;
+    } else {
+        return email.split('_')[1];
+    }
+}
