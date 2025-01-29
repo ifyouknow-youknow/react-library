9b459bc8045c0b573ec7c3533fbe7dd428422a33
ifyouknow-youknow <bagel.inbox@gmail.com>
2025-01-29 00:47:47 -0800

    mach 79

--- src/SERVER/server.js
+++ src/SERVER/server.js
@@ -30,3 +30,32 @@ export async function server_POST(endpoint, args) {
         return null;
     }
 }
+
+export async function GET(url, endpoint) {
+    if (typeof endpoint !== 'string') {
+        console.error("Endpoint must be a string.");
+        return;
+    }
+
+    try {
+        const response = await axios.get(`${url}/${endpoint}`);
+        return response.data; // Return the response data
+    } catch (error) {
+        console.error("Error during server_GET:", error.message);
+    }
+}
+
+export async function POST(url, endpoint, args) {
+    if (typeof endpoint !== 'string') {
+        console.error("Endpoint must be a string.");
+        return;
+    }
+
+    try {
+        const response = await axios.post(`${url}/${endpoint}`, args);
+        return response.data; // Return the object from the response
+    } catch (error) {
+        console.error("Error during server_POST:", error.message);
+        return null;
+    }
+}
