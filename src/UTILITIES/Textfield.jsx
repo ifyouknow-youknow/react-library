66dcb6ad9790fb0121a642271422a7efc07271e1
ifyouknow-youknow <bagel.inbox@gmail.com>
2025-08-02 16:08:58 +0900

    mach 89

--- src/UTILITIES/Textfield.jsx
+++ src/UTILITIES/Textfield.jsx
@@ -1,3 +1,3 @@
-export function Textfield({ id, placeholder, defaultValue, classes = '' }) {
-    return <input type="text" id={id} defaultValue={defaultValue} placeholder={placeholder !== undefined ? placeholder : 'type text here..'} className={`host full-width box no-border ${classes}`} />
+export function Textfield({ id, placeholder, defaultValue, isPassword, classes = '' }) {
+    return <input type={isPassword ? 'password' : 'text'} id={id} defaultValue={defaultValue} placeholder={placeholder !== undefined ? placeholder : 'type text here..'} className={`host full-width box no-border ${classes}`} />
 }
